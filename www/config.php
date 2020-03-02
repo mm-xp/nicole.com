7098e7a38325720e1b760ebd2cd030df90e72f1f
Xopher <xpollard@maddenmedia.com>
2020-03-02 13:58:39 -0700

    :whale: dockerized

--- www/config.php
+++ www/config.php
@@ -1,8 +1,8 @@
 <?php
 
-define('DB_SERVER', 'localhost');
+define('DB_SERVER', db);
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'password');
 define('DB_NAME', 'nicole_com');
 
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
